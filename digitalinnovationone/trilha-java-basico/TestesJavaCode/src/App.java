6e7f52bec2f8f218433a732e87305479ee37ae9d
Alexandre Melo <alexandremelo52@gmail.com>
2025-03-27 00:18:07 -0300

    teste

--- digitalinnovationone/trilha-java-basico/TestesJavaCode/src/App.java
+++ digitalinnovationone/trilha-java-basico/TestesJavaCode/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Entrada do saldo inicial
+        double saldoInicial = scanner.nextDouble();
+
+        // TODO: Na linha abaixo, implemente a entrada das três transações:
+        
+
+        // TODO: Na linha abaixo, realize o cálculo do saldo final:
+        
+        // Saldo final
+        System.out.printf("%.2f\n", saldoFinal);
+
+        scanner.close();
     }
 }
