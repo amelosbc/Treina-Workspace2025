14a8c0562ab98ab94a054c8df515f7829e4d44b0
Alexandre Melo <alexandremelo52@gmail.com>
2025-05-05 23:22:53 -0300

    ajustado projeto

--- digitalinnovationone/desafios/desafio-api-rest/src/main/java/com/amelosbc/desafio_api_rest/domain/model/News.java
+++
@@ -1,8 +0,0 @@
-package com.amelosbc.desafio_api_rest.domain.model;
-
-import jakarta.persistence.Entity;
-
-@Entity(name = "tb_news")
-public class News extends BaseItem {
-    // Nenhum atributo adicional necessário no momento
-}
